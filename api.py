2864d6569dd6d00d3ee85bbb21cc5dea2db9cd5a
Mario Rosero <mroserov@hotmail.com>
2020-05-20 09:22:56 -0500

    swagger doc

--- api.py
+++ api.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/data')
 def hello_world():
-    return 'Api Cedenar'
+    """Example Api
+    """
+    return jsonify({'data':[1,2,3,4,5,6,7,8,9,10]})
+
+@app.route('/')
+def hello(name=None):
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
